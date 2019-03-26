e02d8864ec4a884984a1c7b5d0445c43f8ae2ff3
rinorahmetiii <ahmeti.rinori@gmail.com>
2019-03-26 16:42:12 +0100

    Adding some animations

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
-
-let images = ['img/lapki.jpg' , 'img/varit.jpg' , 'img/architect.jpg'];
-let nextimg = 0;
-slideShow();
-
-function slideShow() {
-    if(nextimg >= images.length) {
-        nextimg = 0;
+$(document).ready(function() {
+  let images = ["img/lapki.jpg", "img/varit.jpg", "img/architect.jpg"];
+  let nextimg = 0;
+  slideShow();
+// Backround img slide
+  function slideShow() {
+    if (nextimg >= images.length) {
+      nextimg = 0;
     }
-    $('#home-section').css("background" , `url(${images[nextimg++]})`).fadeIn(500,function(){
-        setTimeout(slideShow , 8000);
-    });
-}
-
+    $("#home-section")
+      .css("background", `url(${images[nextimg++]})`)
+      .fadeIn(500, function() {
+        setTimeout(slideShow, 8000);
+      });
+  }
+  // Scrool down 
+  $('.shkoPosht').click(function(e) {
+    // Preventing default action of the event
+    e.preventDefault();
+    // Getting the height of the document
+    let n = $(document).height();
+    $('html, body').animate({ scrollTop: n }, 1500);
+//                                       |    |
+//                                       |    --- duration (milliseconds) 
+//                                       ---- distance from the top
+});
+});
 
 // $(function () {
 //     let homeSection = $('#home-section');
 //     let imges = [
-//       'url(img/lapki.jpg)', 
+//       'url(img/lapki.jpg)',
 //       'url(img/varit.jpg)',
 //       'url(img/architect.jpg)'
 //     ];
