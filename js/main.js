99419c0f0597e6639812bc1cb07f52b973c927ec
rinorahmetiii <ahmeti.rinori@gmail.com>
2019-04-25 23:05:49 +0200

    Add

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   let images = ["img/lapki.jpg", "img/varit.jpg", "img/architect.jpg"];
   let nextimg = 0;
-
+  
   slideShow();
-// Backround img slide
+  // Backround img slide
   function slideShow() {
     if (nextimg >= images.length) {
       nextimg = 0;
@@ -14,14 +14,13 @@ $(document).ready(function() {
         setTimeout(slideShow, 8000);
       });
   }
-  
+
   //Center bckgrnd img center top
   document.getElementById("profibuild").style.backgroundPosition = "center top";
 
 
-// Remove light-overlay class when on small devices
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $('.container').removeClass("light-overlay");
- };
-
+  // Remove light-overlay class when on small devices
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $('.container').removeClass("light-overlay");
+  };
 });
