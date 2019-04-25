0eaf336d8b357fcfea0a5d9cefd48d4b18d45760
rinorahmetiii <ahmeti.rinori@gmail.com>
2019-04-25 12:25:11 +0200

    Clean main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   let images = ["img/lapki.jpg", "img/varit.jpg", "img/architect.jpg"];
   let nextimg = 0;
+
   slideShow();
 // Backround img slide
   function slideShow() {
@@ -13,18 +14,9 @@ $(document).ready(function() {
         setTimeout(slideShow, 8000);
       });
   }
+  
+  //Center bckgrnd img center top
   document.getElementById("profibuild").style.backgroundPosition = "center top";
-//   // Scrool down 
-//   $('.shkoPosht').click(function(e) {
-//     // Preventing default action of the event
-//     e.preventDefault();
-//     // Getting the height of the document
-//     let n = $(document).height();
-//     $('html, body').animate({ scrollTop: n }, 1500);
-// //                                       |    |
-// //                                       |    --- duration (milliseconds) 
-// //                                       ---- distance from the top
-// });
 
 
 // Remove light-overlay class when on small devices
@@ -33,22 +25,3 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
  };
 
 });
-
-// $(function () {
-//     let homeSection = $('#home-section');
-//     let imges = [
-//       'url(img/lapki.jpg)',
-//       'url(img/varit.jpg)',
-//       'url(img/architect.jpg)'
-//     ];
-//     let current = 0;
-
-//     function nextBackground() {
-//         homeSection.css(
-//             'background', imges[current = ++current % imges.length]);
-
-//         setTimeout(nextBackground, 5000);
-//     }
-//     setTimeout(nextBackground, 5000);
-//     homeSection.css('background', imges[0]);
-// });
