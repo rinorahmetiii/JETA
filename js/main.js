3fd4be73a1314942ac41b3cc2003620aeae7d356
rinorahmetiii <ahmeti.rinori@gmail.com>
2019-04-30 20:26:23 +0200

    (testmode)Retrieve data from JSON file in another way

--- js/main.js
+++ js/main.js
@@ -9,22 +9,30 @@ $(document).ready(function() {
       nextimg = 0;
     }
     $("#home-section")
-      .css("background", `url(${images[nextimg++]})`)
-      .fadeIn(500, function() {
+      .css("background", `url(${images[nextimg++]})`).fadeIn(500, function() {
         setTimeout(slideShow, 8000);
       });
   }
 
   // Retrieve Projects data from JSON
-  $.ajax({
-    method: "GET",
-    dataType : 'json',
-    url: "projects.json",
-  }).done(function(data) {
-    for(i = 0 ; i < data.length ; i++) {
-      console.log(data[i]);
-    }
+  $.getJSON( "projects.json", function( data ) {
+    let items = [];
+    $.each( data, function( key, val ) {
+      items.push( "<li id='" + key + "'>" + val + "</li>" );
+    });
+   
+    console.log(items);
   });
+
+  // $.ajax({
+  //   method: "GET",
+  //   dataType : 'json',
+  //   url: "projects.json",
+  // }).done(function(data) {
+  //   for(i = 0 ; i < data.length ; i++) {
+  //     console.log(data[i]);
+  //   }
+  // });
   
   // Remove light-overlay class when on small devices
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
