d1ac9517e84713dd88e2067d2ff86fb1061b0fca
rinorahmetiii <ahmeti.rinori@gmail.com>
2019-04-30 21:25:58 +0200

    Display projects

--- js/main.js
+++ js/main.js
@@ -1,11 +1,79 @@
 $(document).ready(function() {
+
   let images = ["img/lapki.jpg", "img/varit.jpg", "img/architect.jpg"];
   let nextimg = 0;
+  let projects = [
+    {
+      id: 1,
+      date: "DECEMBER, 2018",
+      img: "img/portfolio1.png",
+      title: "Pure Bedroom Architecture",
+      description: "If you are looking at blank cassettes on the web lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt."
+    },
+    {
+      id: 2,
+      date: "DECEMBER, 2018",
+      img: "img/portfolio2.png",
+      title: "Pure Bedroom Architecture",
+      description: "If you are looking at blank cassettes on the web lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt."
+    }
+  ];
+
+  getProjects();
 
   slideShow();
 
   removeLightOverlay();
 
+
+  // Get Projects
+  function getProjects() {
+    projects.forEach(function(project) {
+      if(project.id % 2 != 0) {
+        $('.innerProjects').append(`
+          <div class="row align-items-end pb-md-5 mb-4">
+            <div class="col-md-7 mb-4 mb-md-0">
+              <div class="portfolio__img">
+              <img class="img-fluid" src="${project.img}" alt="">
+            </div>
+          </div>
+          <div class="col-md-5 mb-5 pl-xl-5">
+            <h4 class="section-intro__title left-border">${project.date}</h4>
+            <h2 class="section-intro__subtitle small">
+            ${project.title}
+            </h2>
+            <p>
+            ${project.description}
+            </p>
+
+            <a class="btn btn--rightBorder mt-3 btnkostum" href="#/">Read More</a>
+          </div>
+        </div>
+        `);
+      } else {
+        $('.innerProjects').append(`
+          <div class="row align-items-end pb-md-5 mb-4">
+            <div class="col-md-5 mb-5 pl-xl-5">
+              <h4 class="section-intro__title left-border">${project.date}</h4>
+              <h2 class="section-intro__subtitle small">
+              ${project.title}
+              </h2>
+              <p>
+              ${project.description}
+              </p>
+              <a class="btn btn--rightBorder mt-3 btnkostum" href="#/">Read More</a>
+            </div>
+
+            <div class="col-md-7 mb-4 mb-md-0">
+              <div class="portfolio__img">
+                <img class="img-fluid" src="${project.img}" alt="">
+              </div>
+            </div>
+        </div>
+        `);
+      }
+    });
+  }
   
   // Backround img slide
   function slideShow() {
